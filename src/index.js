ce455c1d619287942ffd31a5174d0da61763c2a4
choinashil <choinashil@gmail.com>
2021-01-04 22:54:24 +0900

    Add react method

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Tiny React');
+/* @jsx createElement */
+import { createElement, render } from './react.js';
+
+function Title() {
+  return (
+    <h2>정말 동작할까?</h2>
+  );
+}
+
+render(<Title />, document.querySelector('#root'));
---
+++ src/react.js
@@ -0,0 +1,7 @@
+export function render() {
+
+}
+
+export function createElement() {
+
+}
